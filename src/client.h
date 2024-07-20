c94b1c21aae2cc6328235a8e1b8250d30517c380
Eduardo Ervideira <ejcept@gmail.com>
2024-07-20 23:10:40 +0100

    refactor(client): update function signatures and add set_client_focus

--- src/client.h
+++ src/client.h
@@ -6,11 +6,12 @@
 Client* create_client();
 Client* get_client(WindowManager *gwm, xcb_window_t window);
 int destroy_client(WindowManager *gwm, Client *client);
-int resize_client(WindowManager *gwm, Client *client, uint16_t width, uint16_t height, bool manual_resize);
-int move_client(WindowManager *gwm, Client *client, uint16_t x, uint16_t y);
+int resize_client(WindowManager *gwm, Client *client, uint16_t width, uint16_t height);
+int move_client(WindowManager *gwm, Client *client, int16_t x, int16_t y);
 int get_client_index(WindowManager *gwm, xcb_window_t window);
 int toggle_visibility(WindowManager *gwm, Client *client, int is_visible);
 int toggle_floating(WindowManager *gwm, Client *client, bool is_floating);
+int set_client_focus(WindowManager *gwm, Client *client, int client_index);
 int set_client_border_width(WindowManager *gwm, Client *client, uint16_t border_width);
 int set_client_border_color(WindowManager *gwm, Client *client, uint32_t color);
 int apply_client_rules(WindowManager *gwm, Client *client);
